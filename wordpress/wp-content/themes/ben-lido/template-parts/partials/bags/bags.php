7aea4f975b30aa2c65743b39096faa4b217896c9
Marian Mirzacu <marian.mirzacu@wisesys.ro>
2018-04-11 16:05:06 +0300

    UPBL-4 - #27: Bags Page Changes (WIP)

--- wordpress/wp-content/themes/ben-lido/template-parts/partials/bags/bags.php
+++ wordpress/wp-content/themes/ben-lido/template-parts/partials/bags/bags.php
@@ -42,7 +42,7 @@
                 <?php if($loop%2 ==0): ?><div class="row"><?php endif;?>
                     <div class="col-sm-6 bag-item">
                         <div class="h-100">
-                            <p><a href="#">Details</a></p>
+                            <div class="bag-details-link"><a href="#">Details</a></div>
                             <div class="bag-image hd-8 center">
                                 <img src="<?php echo $product_image;?>" srcset="<?php if (!empty($product_image_retina)) { echo $product_image_retina . ' 2x ';} ?>" alt="<?php echo esc_attr($product_name);?>">
                             </div>
@@ -50,8 +50,15 @@
                                 <h1><?php echo $product_name;?></h1>
                                 <?php echo apply_filters( 'the_content', $bag_product->get_description() );?>
                             </div>
-                            <div class="bag-colors"></div>
-                            <a href="<?php echo $add_to_cart_url;?>" class="generic-button center"><span class="separator">Add | $<?php echo money_format('%!n', $bag_product->get_price()); ?></span></a>
+                        </div>
+                        <div class="bag-footer d-flex justify-content-between">
+                            <span class="price">$<?php echo money_format('%!n', $bag_product->get_price()); ?></span>
+                            <span class="offer"><span class="brown">Free</span> shipping and % off products with purchase</span>
+                            <div class="bag-colors">
+                                <i class="fa fa-circle brown"></i>
+                                <i class="fa fa-circle blue"></i>
+                            </div>
+                            <a href="<?php echo $add_to_cart_url;?>" class="float-right"><i class="fa fa-plus-circle brown"></i></a>
                         </div>
                     </div>
                 <?php $loop++;?>
