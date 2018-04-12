6dd8403da35637134246b88b831f824763000ab9
Marian Mirzacu <marian.mirzacu@wisesys.ro>
2018-04-12 10:17:43 +0300

    UPBL-4 - #27: Bags Page Changes (WIP)

--- wordpress/wp-content/themes/ben-lido/template-parts/partials/bags/bags.php
+++ wordpress/wp-content/themes/ben-lido/template-parts/partials/bags/bags.php
@@ -43,9 +43,11 @@
                     <div class="col-sm-6 bag-item">
                         <div class="card">
                             <div class="card-body">
-                                <div class="bag-details-link"><a href="#">Details</a></div>
+                                <div class="bag-details-link"><a href="<?php echo esc_url( get_permalink($bag_product->get_id()) ); ?>">Details</a></div>
                                 <div class="bag-image hd-8 center">
-                                    <img src="<?php echo $product_image;?>" srcset="<?php if (!empty($product_image_retina)) { echo $product_image_retina . ' 2x ';} ?>" alt="<?php echo esc_attr($product_name);?>">
+                                    <a href="<?php echo esc_url( get_permalink($bag_product->get_id()) ); ?>">
+                                        <img src="<?php echo $product_image;?>" srcset="<?php if (!empty($product_image_retina)) { echo $product_image_retina . ' 2x ';} ?>" alt="<?php echo esc_attr($product_name);?>">
+                                    </a>
                                 </div>
                                 <div class="bag-content">
                                     <h1><?php echo $product_name;?></h1>
