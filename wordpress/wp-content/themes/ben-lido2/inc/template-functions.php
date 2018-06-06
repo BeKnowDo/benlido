fcec12de0dfef2c1f3bf5239bef1ce8ae9f7a52a
Dave <dave@macminis-Mac-mini.local>
2018-06-06 18:55:43 -0400

    a bunch of changes for twig to get it to work with templates

--- wordpress/wp-content/themes/ben-lido2/inc/template-functions.php
+++ wordpress/wp-content/themes/ben-lido2/inc/template-functions.php
@@ -1,13 +1,22 @@
 <?php
 // this file contains functions to get general display elements
 // like nav menu, social media, etc.
+
+function bl_get_site_logo() {
+  $logo = null;
+  if ( function_exists( 'the_custom_logo' ) && has_custom_logo() ) {
+    $logo = get_custom_logo();
+  }
+  return $logo;
+}
+
 function bl_get_top_nav() {
     $final_nav = array();
     $locations = get_nav_menu_locations();
     $menu_id = $locations[ 'primary' ] ;
     $nav = wp_get_nav_menu_items($menu_id);
     foreach ($nav as $item) {
-      $res = array('id'=>$item->ID,'title'=>$item->title,'alt_title'=>esc_attr($item->title),'post_name'=>$item->post_name,'url'=>$item->url,'target'=>$item->target,'classes'=>$item->classes);
+      $res = bl_generate_nav_links($item);
       $final_nav[] = $res;
     }
     return $final_nav;
@@ -19,8 +28,25 @@ function bl_get_social_media_nav() {
     $menu_id = $locations[ 'social-menu' ] ;
     $nav = wp_get_nav_menu_items($menu_id);
     foreach ($nav as $item) {
-      $res = array('id'=>$item->ID,'title'=>$item->title,'alt_title'=>esc_attr($item->title),'post_name'=>$item->post_name,'url'=>$item->url,'target'=>$item->target,'classes'=>$item->classes);
+      $res = bl_generate_nav_links($item);
       $final_nav[] = $res;
     }
     return $final_nav;
+}
+
+function bl_generate_nav_links($item) {
+  $res = array();
+  if (!empty($item) && is_object($item)) {
+    $res = array(
+      'id'=>$item->ID,
+      'title'=>$item->title,
+      'alt_title'=>esc_attr($item->title),
+      'post_name'=>$item->post_name,
+      'url'=>$item->url,
+      'target'=>$item->target,
+      'classes'=>implode(' ', $item->classes)
+    );
+  }
+  
+  return $res;
 }
--- wordpress/wp-content/themes/ben-lido2/template-parts/common/header.php
+++ wordpress/wp-content/themes/ben-lido2/template-parts/common/header.php
@@ -4,11 +4,17 @@
 $list = array();// this is the navigation list
 $socialMediaLinks = array(); // this is the social media nav
 $className = '';
+$logo = array();
+if (function_exists('bl_get_site_logo')) {
+    $logo = bl_get_site_logo();
+}
+
+
 if (function_exists('bl_get_top_nav')) {
     $list = bl_get_top_nav();
 }
 if (function_exists('bl_get_social_media_nav')) {
     $socialMediaLinks = bl_get_social_media_nav();
 }
-$data = array('list'=>$list,'socialMediaLinks'=>$socialMediaLinks);
+$data = array('list'=>$list,'socialMediaLinks'=>$socialMediaLinks,'logo'=>$logo);
 Timber::render( 'common/header.twig', $data);
