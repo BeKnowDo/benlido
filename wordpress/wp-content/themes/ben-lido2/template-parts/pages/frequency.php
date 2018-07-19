7b15b534281f899895d62222fc7a69004fca12bb
Dave <dave@macminis-Mac-mini.local>
2018-07-19 16:15:04 -0400

    added icon to the ACF

--- wordpress/wp-content/themes/ben-lido2/template-parts/pages/frequency.php
+++ wordpress/wp-content/themes/ben-lido2/template-parts/pages/frequency.php
@@ -25,7 +25,11 @@
                     if (empty($number_of_days) || $number_of_days == 0) {
                         $number_of_days = -1;
                     }
-                    $shipping = array('header'=>$button['button_copy'],'number_of_days'=>$number_of_days);
+                    $icon = $button['icon'];
+                    if (!empty($icon) && is_array($icon) && isset($icon['url'])) {
+                        $icon = $icon['url'];
+                    }
+                    $shipping = array('header'=>$button['button_copy'],'image'=>$icon,'number_of_days'=>$number_of_days);
                     $data['shipping'] = $shipping;
                 ?>
                  <div class="column col-4 col-xs-12 col-sm-12">
