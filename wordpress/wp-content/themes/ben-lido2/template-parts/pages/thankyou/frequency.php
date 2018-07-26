6be8b7328073300ea218db18bb433c3fff026534
Dave <dave@macminis-Mac-mini.local>
2018-07-26 09:56:04 -0400

    a bunch of stuff for the frequency tile copy

--- wordpress/wp-content/themes/ben-lido2/template-parts/pages/thankyou/frequency.php
+++ wordpress/wp-content/themes/ben-lido2/template-parts/pages/thankyou/frequency.php
@@ -39,7 +39,7 @@ if (function_exists('get_field')) {
                         if (!empty($icon) && is_array($icon) && isset($icon['url'])) {
                             $icon = $icon['url'];
                         }
-                        $shipping = array('header'=>$button['button_copy'],'image'=>$icon,'number_of_days'=>$number_of_days);
+                        $shipping = array('header'=>$button['button_copy'],'image'=>$icon,'copy'=>$button['frequency_copy'],'number_of_days'=>$number_of_days);
                         $data['shipping'] = $shipping;
                     ?>
                     <div class="column col-4 col-xs-12 col-sm-12">
