92ce904107260deb29478369a489a80a81a35dea
Dave <dave@macminis-Mac-mini.local>
2018-08-16 06:52:37 -0400

    added some CSS to show a logged in nav item versus a logged out item

--- wordpress/wp-content/themes/ben-lido2/template-parts/common/navigation/navbar-desktop.php
+++ wordpress/wp-content/themes/ben-lido2/template-parts/common/navigation/navbar-desktop.php
@@ -1,7 +1,7 @@
 <?php
 // get the main nav
-if (function_exists('bl_get_main_nav')) {
-    $nav = bl_get_main_nav();
+if (function_exists('bl_get_top_nav')) {
+    $nav = bl_get_top_nav();
 }
 $data = array('list'=>$nav);
 Timber::render( 'common/navigation/navbar-desktop.twig',$data);
