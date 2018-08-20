3e90ee73c3eaf44e7051c474d144da61ee9f877c
BKD <cesar@bkd.io>
2018-08-20 14:10:05 -0400

    Bag gallery...can't do much else without overriding everything coming from Woo Commerce. @Dave - we should just customize that and use the originally designed component and call it a day.

--- wordpress/wp-content/themes/ben-lido2/template-parts/pages/bag.php
+++ wordpress/wp-content/themes/ben-lido2/template-parts/pages/bag.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 $show_hero_section = $show_feature_cards = $show_bottom_section = false;
 if (function_exists('get_field')) {
     $show_hero_section = get_field('show_hero_section');
 }
 
 if ($show_hero_section == true) {
-    get_template_part( 'template-parts/partials/bags/hero'); 
+    get_template_part( 'template-parts/partials/bags/hero');
 }
 
 
-get_template_part( 'template-parts/partials/bags/bags'); 
+get_template_part( 'template-parts/partials/bags/bags');
 
 
