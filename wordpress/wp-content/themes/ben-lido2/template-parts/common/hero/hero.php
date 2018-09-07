a760e04d37548901c76c53627a4fdc8c1bc246b5
Dave Lin <lin.dave@gmail.com>
2018-09-07 14:24:44 -0400

    added button copy and url for the twig templates

--- wordpress/wp-content/themes/ben-lido2/template-parts/common/hero/hero.php
+++ wordpress/wp-content/themes/ben-lido2/template-parts/common/hero/hero.php
@@ -59,12 +59,16 @@ if (empty($data)) {
             array(
                 'header' => 'How it Works',
                 'copy' => "Premium, personalized bags packed with the products you love, delivered anywhere, with easy subscription reorders.",
-                'image' => '/images/hero-1.png'
+                'image' => '/images/hero-1.png',
+                'button_copy' => 'click here',
+                'button_url' => 'http://www.benlido.com/'
             ),
             array(
                 'header'=> 'The Brands You Love',
                 'copy'=> "Unlike the aisle at your local retailer, if you want it, we'll have it - and we'll deliver it - right to your doorstep.",
-                'image'=> '/images/hero-2.png'
+                'image'=> '/images/hero-2.png',
+                'button_copy' => 'click here',
+                'button_url' => 'http://www.benlido.com/'
             )
 
         )
--- wordpress/wp-content/themes/ben-lido2/template-parts/pages/about.php
+++ wordpress/wp-content/themes/ben-lido2/template-parts/pages/about.php
@@ -1,4 +1,8 @@
-
 <?php
-  Timber::render('pages/about-us.twig');
-?>
+  $content = array();
+  //$preHeaderText = get_the_title();
+  if (function_exists('get_field')) {
+  }
+  $content = array('preHeaderText' => $preHeaderText);
+  $data = array('heroData' => $content);
+  Timber::render('pages/about-us.twig',$data);
