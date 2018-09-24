ecdaf42a696df5724e73edc76e8aa82787eae1f4
Dave <dave@macminis-Mac-mini.local>
2018-09-24 12:35:41 -0400

    fixed the bottom add kit to cart button

---
+++ wordpress/wp-content/themes/ben-lido2/interactive/src/javascript/components/google-analytics.js
@@ -0,0 +1,26 @@
+export class BLGoogleAnalytics {
+    constructor () {
+    }
+  
+    init () {
+      //console.log('here')
+    }
+
+    addToCart (product) {
+        console.log(product);
+        if (typeof ga == 'function') {
+            ga('ec:addProduct', {
+                'id': product.id,
+                'name': product.name,
+                'category': product.category,
+                'brand': product.brand,
+                'variant': product.variant,
+                'price': product.price,
+                'quantity': product.qty
+              });
+              ga('ec:setAction', 'add');
+              ga('send', 'event', 'UX', 'click', 'add to cart');     // Send data using an event.
+            
+        }
+    }
+}
