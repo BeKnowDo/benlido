670340f1715f40097f0e8baf30ab6dd0cc993a1a
Dave Lin <David.Lin@ibm.com>
2019-04-24 14:35:13 -0400

    added a plain (non-JSX version) of the same basel header block because there is a bug with media image block

--- wordpress/wp-content/plugins/bl-gutenberg-blocks/bl-gutenberg-blocks.php
+++ wordpress/wp-content/plugins/bl-gutenberg-blocks/bl-gutenberg-blocks.php
@@ -22,4 +22,5 @@ add_filter( 'block_categories', 'bl_gutenberg_block_category', 10, 2);
 // using https://github.com/ahmadawais/create-guten-block
 
 // guten blocks:
-include_once ( plugin_dir_path(__FILE__) . '/blocks/bl-basel-header/src/init.php');
+//include_once ( plugin_dir_path(__FILE__) . '/blocks/bl-basel-header/src/init.php');
+include_once ( plugin_dir_path(__FILE__) . '/blocks/bl-basel-header-plain/src/init.php');
